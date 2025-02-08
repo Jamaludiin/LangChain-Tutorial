51c338b3c3e54491fcbd9080de766a3823a5a0a7
jamal <jamalkacade@gmail.com>
2025-02-08 23:32:15 +0800

    output parser lesson

---
+++ LangChain and Ollama/Lessons/lesson3_output-parsers.py
@@ -0,0 +1,82 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser, CommaSeparatedListOutputParser, JsonOutputParser
+from langchain_core.pydantic_v1 import BaseModel, Field
+
+
+from langchain_core.prompts import ChatPromptTemplate
+import os
+from groq import Groq
+from langchain_groq import ChatGroq
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv()
+
+
+groq_api_key = os.getenv('GROQ_API_KEY')
+
+llm = ChatGroq(
+        model="llama-3.3-70b-versatile",
+        temperature=0.7,
+        max_tokens=32767,
+        timeout=10,
+        max_retries=2,
+    )
+
+
+
+def call_string_output_parser():
+    prompt = ChatPromptTemplate.from_messages([
+        ("system", "Tell me a joke about the following subject"),
+        ("human", "{input}")
+    ])
+
+    parser = StrOutputParser()
+
+    chain = prompt | llm | parser
+
+    return chain.invoke({
+        "input": "dog"
+    })
+
+def call_list_output_parser():
+    prompt = ChatPromptTemplate.from_messages([
+        ("system", "Generate a list of 10 synonyms for the following word. Return the results as a comma seperated list."),
+        ("human", "{input}")
+    ])
+
+    parser = CommaSeparatedListOutputParser()
+    
+    chain = prompt | llm | parser
+
+    return chain.invoke({
+        "input": "happy"
+    })
+
+def call_json_output_parser():
+    prompt = ChatPromptTemplate.from_messages([
+        ("system", "Extract information from the following phrase.\nFormatting Instructions: {format_instructions}"),
+        ("human", "{phrase}")
+    ])
+
+    class Person(BaseModel):
+        recipe: str = Field(description="the name of the recipe")
+        ingredients: list = Field(description="ingredients")
+        
+
+    parser = JsonOutputParser(pydantic_object=Person)
+
+    chain = prompt | llm | parser
+    
+    return chain.invoke({
+        "phrase": "The ingredients for a Margherita pizza are tomatoes, onions, cheese, basil",
+        "format_instructions": parser.get_format_instructions()
+    })
+
+# print(type(call_string_output_parser()))
+# print(type(call_list_output_parser()))
+print(call_json_output_parser())
