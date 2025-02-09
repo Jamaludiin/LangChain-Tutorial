7318712657113744365277639fbcd88347022436
jamal <jamalkacade@gmail.com>
2025-02-09 12:01:02 +0800

    Vision model practice : The following are code examples for passing your image to the model via a URL

--- LangChain and Ollama/Lessons/lesson3_output-parsers.py
+++ LangChain and Ollama/Lessons/lesson3_output-parsers.py
@@ -28,7 +28,7 @@ llm = ChatGroq(
     )
 
 
-
+# 1: StrOutputParser
 def call_string_output_parser():
     prompt = ChatPromptTemplate.from_messages([
         ("system", "Tell me a joke about the following subject"),
@@ -43,6 +43,7 @@ def call_string_output_parser():
         "input": "dog"
     })
 
+# 2: CommaSeparatedListOutputParser
 def call_list_output_parser():
     prompt = ChatPromptTemplate.from_messages([
         ("system", "Generate a list of 10 synonyms for the following word. Return the results as a comma seperated list."),
@@ -57,6 +58,8 @@ def call_list_output_parser():
         "input": "happy"
     })
 
+
+# 3: JsonOutputParser
 def call_json_output_parser():
     prompt = ChatPromptTemplate.from_messages([
         ("system", "Extract information from the following phrase.\nFormatting Instructions: {format_instructions}"),
@@ -76,6 +79,7 @@ def call_json_output_parser():
         "phrase": "The ingredients for a Margherita pizza are tomatoes, onions, cheese, basil",
         "format_instructions": parser.get_format_instructions()
     })
+
 
 # print(type(call_string_output_parser()))
 # print(type(call_list_output_parser()))
