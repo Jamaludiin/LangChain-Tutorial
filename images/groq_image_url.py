26dfe38a495461eff08d0b7758e170af20a0993a
jamal <jamalkacade@gmail.com>
2025-03-09 13:01:06 +0800

    image description generation

---
+++ images/groq_image_url.py
@@ -0,0 +1,37 @@
+# error
+
+from groq import Groq
+from groq import Groq
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv()
+
+client = Groq()
+completion = client.chat.completions.create(
+    model="llama-3.2-11b-vision-preview",
+    messages=[
+        {
+            "role": "user",
+            "content": [
+                {
+                    "type": "text",
+                    "text": "What's in this image?"
+                },
+                {
+                    "type": "image_url",
+                    "image_url": {
+                        "url": "https://upload.wikimedia.org/wikipedia/commons/f/f2/LPU-v1-die.jpg"
+                    }
+                }
+            ]
+        }
+    ],
+    temperature=1,
+    max_completion_tokens=1024,
+    top_p=1,
+    stream=False,
+    stop=None,
+)
+
+print(completion.choices[0].message)
